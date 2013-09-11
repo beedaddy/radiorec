24b0956280e2cf8f7c74bf8c778955a2a24827fd
Martin Brodbeck <martin@brodbeck-online.de>
2013-09-11 16:10:16 +0200

    Nothing noteworthy

--- radiorec.py
+++ radiorec.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 
 """
 This script records internet radio streams. It can be used in conjunction
@@ -9,3 +10,8 @@ import argparse
 import configparser
 import urllib.request
 
+def main():
+    # TODO: Implement argparse
+
+if __name__ == '__main__':
+    main()
