1dad726c2e280eddb29f21b4b22d68703e3b1bb6
Martin Brodbeck <martin@brodbeck-online.de>
2013-09-11 11:06:13 +0200

    Initial description

--- radiorec.py
+++ radiorec.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
+"""
+This script records internet radio streams. It can be used in conjunction
+with "at" or "crontab".
+"""
+
 import argparse
 import configparser
 import urllib.request
-
 
