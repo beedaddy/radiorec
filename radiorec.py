41394fa0278b7d2586c53fa3d2dd4d8a8f358cbb
Martin Brodbeck <martin@brodbeck-online.de>
2013-09-12 15:23:00 +0200

    Added some arguments

--- radiorec.py
+++ radiorec.py
@@ -11,7 +11,12 @@ import configparser
 import urllib.request
 
 def main():
-    # TODO: Implement argparse
+    parser = argparse.ArgumentParser(description='This program records internet radio streams.')
+    parser.add_argument('station', type=str, help='Name of the radio station '
+                                                '(see config file for a list)')
+    parser.add_argument('duration', type=int, help='Recording time in minutes')
+    parser.add_argument('name', nargs='?', type=str, help='A name for the recording')
+    args = parser.parse_args()
 
 if __name__ == '__main__':
     main()
