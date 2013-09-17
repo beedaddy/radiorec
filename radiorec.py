435ffb827ad6b1a540aebcbfa95b4054a34c274b
Martin Brodbeck <martin@brodbeck-online.de>
2013-09-17 10:09:38 +0200

    Added support for M3U playlists\nAdded verbose output

--- radiorec.py
+++ radiorec.py
@@ -47,28 +47,39 @@ def record_worker(stoprec, streamurl, target_dir, name=None):
     elif(content_type == 'application/ogg' or content_type == 'audio/ogg'):
         filename += '.ogg'
     elif(content_type == 'audio/x-mpegurl'):
-        #TODO Add support for M3U playlists
         print('Sorry, M3U playlists are currently not supported')
         sys.exit()
     else:
         print('Unknown content type "' + content_type + '". Assuming mp3.')
         filename += 'mp3'
-    target = open(filename, "wb")
-    while(not stoprec.is_set() and not conn.closed):
-        target.write(conn.read(1024))
+    with open(filename, "wb") as target:
+        while(not stoprec.is_set() and not conn.closed):
+            target.write(conn.read(1024))
 
 def record(args):
     settings = read_settings()
     streamurl = ''
+    global verboseprint
+    verboseprint = print if args.verbose else lambda *a, **k: None
+
     try:
         streamurl = settings['STATIONS'][args.station]
     except KeyError:
         print('Unkown station name: ' + args.station)
-        return
+        sys.exit()
+    if streamurl.endswith('.m3u'):
+        verboseprint('Seems to be an M3U playlist. Trying to parse…')
+        with urllib.request.urlopen(streamurl) as remotefile:
+            for line in remotefile:
+                if not line.decode('utf-8').startswith('#'):
+                    tmpstr = line.decode('utf-8')
+                    break
+        streamurl = tmpstr
+    verboseprint('stream url: ' + streamurl)
     target_dir = os.path.expandvars(settings['GLOBAL']['target_dir'])
     stoprec = threading.Event()
 
-    print('Recording ' + args.station + '…')
+    verboseprint('Recording ' + args.station + '…')
     recthread = threading.Thread(target = record_worker, 
                         args = (stoprec, streamurl, target_dir, args.name), daemon = True)
     recthread.start()
@@ -79,7 +90,6 @@ def record(args):
 
 def list(args):
     settings = read_settings()
-    print('Known stations:')
     for key in settings['STATIONS']:
         print(key)
 
@@ -93,6 +103,7 @@ def main():
                         help='Recording time in minutes')
     parser_record.add_argument('name', nargs='?', type=str, 
                         help='A name for the recording')
+    parser_record.add_argument('-v', '--verbose', action='store_true', help="Verbose output")
     parser_record.set_defaults(func=record)
     parser_list = subparsers.add_parser('list', help='List all known stations')
     parser_list.set_defaults(func=list)
